241d412777e71ed13034ea079627cd24c9f7b2cf
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2018-09-14 15:43:17 -0400

    Allow functions with no args

--- TabbyScript/Nodes/Scripts/Func.gd
+++ TabbyScript/Nodes/Scripts/Func.gd
@@ -13,14 +13,15 @@ func call(args):
 		if data.type == Tabby.PTR:
 			pointers.append(data)
 
-	if len(args) > len(pointers):
+	if len(args) > len(pointers) and len(pointers) != 0:
 		return Tabby.throw('To many arguments passed to function', self.line_number)
 
 	elif len(args) < len(pointers):
 		return Tabby.throw('Not enough arguments passed to function', self.line_number)
 
-	for index in len(pointers):
-		self.Variables[pointers[index].data] = args[index]
+	if len(pointers) != 0:
+		for index in len(pointers):
+			self.Variables[pointers[index].data] = args[index]
 
 	execute_children()
 	return Tabby.malloc(Tabby.SUC)
