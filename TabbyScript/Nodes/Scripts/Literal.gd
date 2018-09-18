e075625f8eb8efe26f40dc72faded7af30b62c27
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2018-09-17 21:05:31 -0400

    Optimize Literal.gd

--- TabbyScript/Nodes/Scripts/Literal.gd
+++ TabbyScript/Nodes/Scripts/Literal.gd
@@ -4,9 +4,4 @@ const Type = 'data'
 var Data = null
 
 func get_data():
-	var data = self.Data
-
-	if data.type == Tabby.NUM:
-		data.data = Tabby.check_float(data.data)
-
-	return data
+	return self.Data
