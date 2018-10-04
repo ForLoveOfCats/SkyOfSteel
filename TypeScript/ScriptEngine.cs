313d54ac923808e958bfe5103bfae34092762d5a
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2018-10-04 13:31:56 -0400

    Basic script api

--- TypeScript/ScriptEngine.cs
+++ TypeScript/ScriptEngine.cs
@@ -4,9 +4,9 @@ using Jurassic;
 
 public class ScriptEngine : Node
 {
-	public override void _Ready()
+	public void RunNewScript(string Script)
 	{
-		var engine = new Jurassic.ScriptEngine();
-		Console.WriteLine(engine.Evaluate("5 * 10 + 2"));
+		Jurassic.ScriptEngine Engine = new Jurassic.ScriptEngine();
+		GD.Print(Engine.Evaluate(Script));
 	}
 }
