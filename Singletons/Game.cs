d60e26be0be4bbb6d850b3b302604fb35ea9a8d8
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2018-10-09 15:40:31 -0400

    Add Game.cs missing from last commit

---
+++ Singletons/Game.cs
@@ -0,0 +1,75 @@
+using Godot;
+using System;
+
+public class Game : Node
+{
+	private static Node SteelGame;
+
+	public static int MaxPlayers = 8;
+	public static bool MouseLocked = false;
+	public static bool PlayerInputEnabled = true;
+
+	private static Game Self;
+	private Game()
+	{
+		Self = this;
+	}
+
+
+	public override void _Ready()
+	{
+		SteelGame = GetTree().GetRoot().GetNode("SteelGame");
+	}
+
+
+	public static void Set(string Object, bool Data)  //Temporary
+	{
+		switch(Object)
+		{
+			case "MouseLocked":
+				MouseLocked = Data;
+				break;
+			case "PlayerInputEnabled":
+				PlayerInputEnabled = Data;
+				break;
+		}
+	}
+
+
+	public static void SpawnPlayer(string Id, bool Possess)
+	{
+		Node Player = ((PackedScene)GD.Load("res://scenes/Player.tscn")).Instance();
+		Player.Set("possessed", Possess);
+		Player.SetName(Id);
+		SteelGame.GetNode("SkyScene").AddChild(Player);
+	}
+
+
+	public static void CloseWorld()
+	{
+		if(SteelGame.HasNode("SkyScene"))
+		{
+			SteelGame.GetNode("SkyScene").QueueFree();
+		}
+	}
+
+
+	public static void StartWorld()
+	{
+		CloseWorld();
+		Node World = ((PackedScene)GD.Load("res://scenes/SkyScene.tscn")).Instance();
+		World.SetName("SkyScene");
+		SteelGame.AddChild(World);
+
+		for(int X = 0; X <= 10; X++)
+		{
+			for(int Y = 0; Y <= 10; Y++)
+			{
+				Spatial Platform = (Spatial)((PackedScene)GD.Load("res://scenes/structures/Platform.tscn")).Instance();
+				Platform.Translate(new Vector3(X*12,0,Y*12));
+				World.AddChild(Platform);
+			}
+		}
+	}
+
+}
