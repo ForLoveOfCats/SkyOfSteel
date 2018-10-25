0332ec3fca56ad98e53160ef3d1e1e4b9a1048fd
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2018-10-25 15:29:28 -0400

    Player movement!

--- World/Player.cs
+++ World/Player.cs
@@ -6,8 +6,14 @@ public class Player : Spatial
 {
 	public bool Possessed = false;
 
+	private const float BaseMovementSpeed = 16;
+	private const float MovementInputMultiplyer = BaseMovementSpeed;
+	private const float MaxMovementSpeed = 90;
+	private const float Friction = BaseMovementSpeed*10;
+
 	private int ForwardAxis = 0;
 	private int RightAxis = 0;
+	private Vector3 Momentum = new Vector3(0,0,0);
 
 	public override void _Ready()
 	{
@@ -22,8 +28,14 @@ public class Player : Spatial
 
 	public void ForwardMove(double Sens)
 	{
+		if(!Game.PlayerInputEnabled)
+		{
+			return;
+		}
+
 		if(Sens > 0d)
 		{
+			Momentum.z = Mathf.Clamp((float)(Sens*MovementInputMultiplyer), 0f, BaseMovementSpeed);
 			ForwardAxis = 1;
 		}
 		else if(ForwardAxis > 0)
@@ -35,8 +47,14 @@ public class Player : Spatial
 
 	public void BackwardMove(double Sens)
 	{
+		if(!Game.PlayerInputEnabled)
+		{
+			return;
+		}
+
 		if(Sens > 0d)
 		{
+			Momentum.z = Mathf.Clamp((float)(-1*Sens*MovementInputMultiplyer), -BaseMovementSpeed, 0f);
 			ForwardAxis = -1;
 		}
 		else if(ForwardAxis < 0)
@@ -48,8 +66,14 @@ public class Player : Spatial
 
 	public void RightMove(double Sens)
 	{
+		if(!Game.PlayerInputEnabled)
+		{
+			return;
+		}
+
 		if(Sens > 0d)
 		{
+			Momentum.x = Mathf.Clamp((float)(-1*Sens*MovementInputMultiplyer), -BaseMovementSpeed, 0f);
 			RightAxis = 1;
 		}
 		else if(RightAxis > 0)
@@ -61,8 +85,14 @@ public class Player : Spatial
 
 	public void LeftMove(double Sens)
 	{
+		if(!Game.PlayerInputEnabled)
+		{
+			return;
+		}
+
 		if(Sens > 0d)
 		{
+			Momentum.x = Mathf.Clamp((float)(Sens*MovementInputMultiplyer), 0f, BaseMovementSpeed);
 			RightAxis = -1;
 		}
 		else if(RightAxis < 0)
@@ -74,6 +104,32 @@ public class Player : Spatial
 
 	public override void _Process(float Delta)
 	{
-		GD.Print(ForwardAxis, RightAxis);
+		if(ForwardAxis == 0)
+		{
+			if(Momentum.z > 0)
+			{
+				Momentum.z = Mathf.Clamp(Momentum.z-Friction*Delta, 0f, MaxMovementSpeed);
+			}
+			else if (Momentum.z < 0)
+			{
+				Momentum.z = Mathf.Clamp(Momentum.z+Friction*Delta, -MaxMovementSpeed, 0f);
+			}
+		}
+
+		if(RightAxis == 0)
+		{
+			if(Momentum.x > 0)
+			{
+				Momentum.x = Mathf.Clamp(Momentum.x-Friction*Delta, 0f, MaxMovementSpeed);
+			}
+			else if (Momentum.x < 0)
+			{
+				Momentum.x = Mathf.Clamp(Momentum.x+Friction*Delta, -MaxMovementSpeed, 0f);
+			}
+		}
+
+		Translate(Momentum*Delta);
+
+		GD.Print(Momentum);
 	}
 }
