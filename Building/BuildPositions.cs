2197f7e5eb43fefff0757484166e78a0846d5567
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2018-11-16 11:07:19 -0500

    Each branch should have a switch for supported base not other way around

--- Building/BuildPositions.cs
+++ Building/BuildPositions.cs
@@ -3,15 +3,15 @@ using Godot;
 
 public class BuildPositions
 {
-	private void MissingCaseError(Structure Base, Items.TYPE Branch)
+	private void MissingCaseError(Structure Base, Items.TYPE BranchType)
 	{
-		Console.Log("ERROR: Missing position case for '" + Branch.ToString() + "' *ON* '" + Base.Type.ToString() + "'");
+		Console.Log("ERROR: Missing position case for branch '" + BranchType.ToString() + "' on base '" + Base.Type.ToString() + "'");
 	}
 
 
-	private Vector3 OnPlatform(Structure Base, Items.TYPE Branch)
+	private Vector3 PlatformBranch(Structure Base, Items.TYPE BranchType)
 	{
-		switch(Branch)
+		switch(Base.Type)
 		{
 			case(Items.TYPE.PLATFORM):
 				float RotationDegrees = Mathf.Deg2Rad(SteelMath.SnapToGrid(Game.PossessedPlayer.RotationDegrees.y, 360, 4));
@@ -19,22 +19,22 @@ public class BuildPositions
 
 
 			default:
-				MissingCaseError(Base, Branch);
+				MissingCaseError(Base, BranchType);
 				return Base.Translation;
 		}
 	}
 
 
-	public Vector3 Calculate(Structure Base, Items.TYPE Branch)
+	public Vector3 Calculate(Structure Base, Items.TYPE BranchType)
 	{
-		switch(Base.Type)
+		switch(BranchType)
 		{
 			case(Items.TYPE.PLATFORM):
-				return OnPlatform(Base, Branch);
+				return PlatformBranch(Base, BranchType);
 
 
 			default:
-				Console.Log("ERROR: Missing position switch for '" + Base.Type.ToString() + "' *AS* base");
+				Console.Log("ERROR: Missing position switch for branch '" + BranchType.ToString() + "'");
 				return Base.Translation;
 		}
 	}
--- Building/BuildRotations.cs
+++ Building/BuildRotations.cs
@@ -3,37 +3,37 @@ using Godot;
 
 public class BuildRotations
 {
-	private void MissingCaseError(Structure Base, Items.TYPE Branch)
+	private void MissingCaseError(Structure Base, Items.TYPE BranchType)
 	{
-		Console.Log("ERROR: Missing rotation case for '" + Branch.ToString() + "' *ON* '" + Base.Type.ToString() + "'");
+		Console.Log("ERROR: Missing rotation case for branch '" + BranchType.ToString() + "' on base '" + Base.Type.ToString() + "'");
 	}
 
 
-	private Vector3 OnPlatform(Structure Base, Items.TYPE Branch)
+	private Vector3 PlatformBranch(Structure Base, Items.TYPE BranchType)
 	{
-		switch(Branch)
+		switch(Base.Type)
 		{
 			case(Items.TYPE.PLATFORM):
 				return new Vector3(0,0,0);
 
 
 			default:
-				MissingCaseError(Base, Branch);
+				MissingCaseError(Base, BranchType);
 				return Base.RotationDegrees;
 		}
 	}
 
 
-	public Vector3 Calculate(Structure Base, Items.TYPE Branch)
+	public Vector3 Calculate(Structure Base, Items.TYPE BranchType)
 	{
-		switch(Base.Type)
+		switch(BranchType)
 		{
 			case(Items.TYPE.PLATFORM):
-				return OnPlatform(Base, Branch);
+				return PlatformBranch(Base, BranchType);
 
 
 			default:
-				Console.Log("ERROR: Missing rotation switch for '" + Base.Type.ToString() + "' *AS* base");
+				Console.Log("ERROR: Missing rotation switch for branch '" + BranchType.ToString() + "'");
 				return Base.RotationDegrees;
 		}
 	}
