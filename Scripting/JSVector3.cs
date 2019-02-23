4b361307b00ff30b546645217d4e06824cc72dfa
RedstoneParadox <spinjitzu00@gmail.com>
2019-02-23 13:57:54 -0800

    Renamed JSVector3 to JsVector3
    Name change to follow coding conventions.

--- Scripting/JSVector3.cs
+++ Scripting/JSVector3.cs
@@ -1,32 +1,32 @@
 using Jurassic;
 using Jurassic.Library;
 
-public class JSVector3Constructor : ClrFunction
+public class JsVector3Constructor : ClrFunction
 {
 
-    public JSVector3Constructor(ScriptEngine engine) 
-    : base(engine.Function.InstancePrototype, "Vector3", new JSVector3Instance(engine.Object.InstancePrototype))
+    public JsVector3Constructor(ScriptEngine engine) 
+    : base(engine.Function.InstancePrototype, "Vector3", new JsVector3Instance(engine.Object.InstancePrototype))
     {
 
     }
 
     [JSConstructorFunction]
-    public JSVector3Instance Construct(double x, double y, double z) {
-        return new JSVector3Instance(this.InstancePrototype, x, y, z);
+    public JsVector3Instance Construct(double x, double y, double z) {
+        return new JsVector3Instance(this.InstancePrototype, x, y, z);
     }
 
 }
 
-public class JSVector3Instance : ObjectInstance
+public class JsVector3Instance : ObjectInstance
 {
 
-    public JSVector3Instance(ObjectInstance prototype) : base(prototype) {
+    public JsVector3Instance(ObjectInstance prototype) : base(prototype) {
         this["x"] = 0;
         this["y"] = 0;
         this["z"] = 0;
     }
     
-    public JSVector3Instance(ObjectInstance prototype,double x, double y, double z) : base(prototype) {
+    public JsVector3Instance(ObjectInstance prototype,double x, double y, double z) : base(prototype) {
         this["x"] = x;
         this["y"] = y;
         this["z"] = z;
