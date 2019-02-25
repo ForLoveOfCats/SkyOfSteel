38f6a4d22cfc20714a434803816892a7ef34b9e7
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2019-02-25 15:24:45 -0500

    Rename JsVector3Instance to JsVector3

--- Scripting/Types/JsVector3.cs
+++ Scripting/Types/JsVector3.cs
@@ -6,28 +6,28 @@ using Godot;
 public class JsVector3Constructor : ClrFunction
 {
 	public JsVector3Constructor(ScriptEngine engine)
-		: base(engine.Function.InstancePrototype, "Vector3", new JsVector3Instance(engine.Object.InstancePrototype))
+		: base(engine.Function.InstancePrototype, "Vector3", new JsVector3(engine.Object.InstancePrototype))
 	{}
 
 
 	[JSConstructorFunction]
-	public JsVector3Instance Construct()
+	public JsVector3 Construct()
 	{
-		return new JsVector3Instance(this.InstancePrototype);
+		return new JsVector3(this.InstancePrototype);
 	}
 
 
 	[JSConstructorFunction]
-	public JsVector3Instance Construct(double x, double y, double z)
+	public JsVector3 Construct(double x, double y, double z)
 	{
-		return new JsVector3Instance(this.InstancePrototype, x, y, z);
+		return new JsVector3(this.InstancePrototype, x, y, z);
 	}
 }
 
 
-public class JsVector3Instance : ObjectInstance
+public class JsVector3 : ObjectInstance
 {
-	public JsVector3Instance(ObjectInstance prototype) : base(prototype)
+	public JsVector3(ObjectInstance prototype) : base(prototype)
 	{
 		this["x"] = 0;
 		this["y"] = 0;
@@ -35,7 +35,7 @@ public class JsVector3Instance : ObjectInstance
 	}
 
 
-	public JsVector3Instance(ObjectInstance prototype, double x, double y, double z) : base(prototype)
+	public JsVector3(ObjectInstance prototype, double x, double y, double z) : base(prototype)
 	{
 		this["x"] = x;
 		this["y"] = y;
