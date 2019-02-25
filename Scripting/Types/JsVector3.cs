0c5514578ee5c98664f3d221a850844cb24eb98b
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2019-02-25 14:17:17 -0500

    Fix indentation in JsVector3

--- Scripting/Types/JsVector3.cs
+++ Scripting/Types/JsVector3.cs
@@ -5,42 +5,39 @@ using Godot;
 
 public class JsVector3Constructor : ClrFunction
 {
-    public JsVector3Constructor(ScriptEngine engine)
+	public JsVector3Constructor(ScriptEngine engine)
 		: base(engine.Function.InstancePrototype, "Vector3", new JsVector3Instance(engine.Object.InstancePrototype))
-    {
-
-    }
+	{}
 
 
-    [JSConstructorFunction]
-    public JsVector3Instance Construct(double x, double y, double z)
-    {
-        return new JsVector3Instance(this.InstancePrototype, x, y, z);
-    }
-
+	[JSConstructorFunction]
+	public JsVector3Instance Construct(double x, double y, double z)
+	{
+		return new JsVector3Instance(this.InstancePrototype, x, y, z);
+	}
 }
 
 
 public class JsVector3Instance : ObjectInstance
 {
-    public JsVector3Instance(ObjectInstance prototype) : base(prototype)
-    {
-        this["x"] = 0;
-        this["y"] = 0;
-        this["z"] = 0;
-    }
-
-
-    public JsVector3Instance(ObjectInstance prototype, double x, double y, double z) : base(prototype)
+	public JsVector3Instance(ObjectInstance prototype) : base(prototype)
 	{
-        this["x"] = x;
-        this["y"] = y;
-        this["z"] = z;
-    }
+		this["x"] = 0;
+		this["y"] = 0;
+		this["z"] = 0;
+	}
 
 
-    public Vector3 Sharpen()
-    {
-        return new Vector3((float)this["x"], (float)this["y"], (float)this["z"]);
-    }
+	public JsVector3Instance(ObjectInstance prototype, double x, double y, double z) : base(prototype)
+	{
+		this["x"] = x;
+		this["y"] = y;
+		this["z"] = z;
+	}
+
+
+	public Vector3 Sharpen()
+	{
+		return new Vector3((float)this["x"], (float)this["y"], (float)this["z"]);
+	}
 }
