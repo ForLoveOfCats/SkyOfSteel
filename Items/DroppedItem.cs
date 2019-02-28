1a7654bbab8137c83858198fd9d25f6aa6691c17
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2019-02-28 08:37:19 -0500

    Add rotation to DroppedItem

--- Items/DroppedItem.cs
+++ Items/DroppedItem.cs
@@ -4,6 +4,8 @@ using System;
 
 public class DroppedItem : KinematicBody
 {
+	private const float RPS = 0.5f; //Revolutions Per Second
+
 	public Items.TYPE Type = Items.TYPE.ERROR;
 
 	public override void _Ready()
@@ -12,5 +14,11 @@ public class DroppedItem : KinematicBody
 		Mat.Shader = Items.StructureShader;
 		Mat.SetShaderParam("texture_albedo", Items.Textures[Type]);
 		GetNode<MeshInstance>("MeshInstance").MaterialOverride = Mat;
+	}
+
+
+	public override void _PhysicsProcess(float Delta)
+	{
+		SetRotationDegrees(new Vector3(0, RotationDegrees.y+(360*Delta*RPS), 0));
 	}
 }
