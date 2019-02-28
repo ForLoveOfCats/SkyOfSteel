ac4de33261b03384374bd931252cdf9c88c65269
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2019-02-28 16:15:41 -0500

    Cap item fall speed

--- Items/DroppedItem.cs
+++ Items/DroppedItem.cs
@@ -5,6 +5,7 @@ using System;
 public class DroppedItem : KinematicBody
 {
 	private const float Gravity = 14f;
+	private const float MaxFallSpeed = -40f;
 	private const float RPS = 0.5f; //Revolutions Per Second
 
 	public Vector3 Momentum; //Needs to be set when created or else will crash with NullReferenceException
@@ -32,6 +33,10 @@ public class DroppedItem : KinematicBody
 			if(PhysicsEnabled)
 			{
 				Momentum.y -= Gravity*Delta;
+				if(Momentum.y < MaxFallSpeed)
+				{
+					Momentum.y = MaxFallSpeed;
+				}
 			}
 			else
 			{
