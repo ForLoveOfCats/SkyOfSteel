d244b7ac953d48ede173b2c31b08b06a25e56597
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2019-03-11 18:52:24 -0400

    Rename GridClass area calculation methods

--- World/Grid.cs
+++ World/Grid.cs
@@ -18,7 +18,7 @@ public class GridClass
 	private Dictionary<Vector3, List<IInGrid>> Dict = new Dictionary<Vector3, List<IInGrid>>();
 
 
-	private Vector3 GetArea(Vector3 Position)
+	private Vector3 CalculateArea(Vector3 Position)
 	{
 		return new Vector3(FloorToInt(Position.x/PlatformSize)*PlatformSize,
 						   FloorToInt(Position.y/PlatformSize)*PlatformSize,
@@ -26,18 +26,18 @@ public class GridClass
 	}
 
 
-	private List<Vector3> GetAreas(Vector3 Position)
+	private List<Vector3> CalculateAreas(Vector3 Position)
 	{
 		List<Vector3> Out = new List<Vector3>();
-		Out.Add(GetArea(Position));
+		Out.Add(CalculateArea(Position));
 
 		if(Position.x % PlatformSize != 0)
 		{
-			Out.Add(GetArea(Position + new Vector3(PlatformSize/2,0,0)));
+			Out.Add(CalculateArea(Position + new Vector3(PlatformSize/2,0,0)));
 		}
 		else if(Position.z % PlatformSize != 0)
 		{
-			Out.Add(GetArea(Position + new Vector3(0,0,PlatformSize/2)));
+			Out.Add(CalculateArea(Position + new Vector3(0,0,PlatformSize/2)));
 		}
 
 		return Out;
@@ -47,7 +47,7 @@ public class GridClass
 	public void AddItem(IInGrid Item)
 	{
 		List<IInGrid> Items;
-		foreach(Vector3 Area in GetAreas(Item.Translation))
+		foreach(Vector3 Area in CalculateAreas(Item.Translation))
 		{
 			Dict.TryGetValue(Area, out Items);
 			if(Items == null)
@@ -65,7 +65,7 @@ public class GridClass
 
 	public void RemoveItem(IInGrid Item)
 	{
-		foreach(Vector3 Area in GetAreas(Item.Translation))
+		foreach(Vector3 Area in CalculateAreas(Item.Translation))
 		{
 			List<IInGrid> Items;
 			Dict.TryGetValue(Area, out Items);
