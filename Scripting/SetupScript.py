7f459b21cf8013a7e559cdc8cc2aeb1fc1b54db5
finn <finnandshadow@gmail.com>
2019-03-12 13:35:25 -0400

    Actually added the controller bindings to the game (because I forgot to last time)

--- Scripting/SetupScript.py
+++ Scripting/SetupScript.py
@@ -13,32 +13,54 @@ import Items
 import World
 import Menu
 import Startup
+import Constants
 
 
-bind("player_input_forward", "W")
-bind("player_input_backward", "S")
-bind("player_input_right", "D")
-bind("player_input_left", "A")
-bind("player_input_sprint", "Shift")
-bind("player_input_jump", "Space")
-bind("player_input_crouch", "Control")
+#bind("player_input_forward", "W")
+bind_controller_axis("player_input_forward", "W", Constants.XBOX_LS_VERTICAL, Constants.JOYSTICK_UP)
+#bind("player_input_backward", "S")
+bind_controller_axis("player_input_backward", "S", Constants.XBOX_LS_VERTICAL, Constants.JOYSTICK_DOWN)
+#bind("player_input_right", "D")
+bind_controller_axis("player_input_right", "D", Constants.XBOX_LS_HORIZONTAL, Constants.JOYSTICK_RIGHT)
+#bind("player_input_left", "A")
+bind_controller_axis("player_input_left", "A", Constants.XBOX_LS_HORIZONTAL, Constants.JOYSTICK_LEFT)
+#bind("player_input_sprint", "Shift")
 
-bind("player_input_inventory_up", "WheelUp")
-bind("player_input_inventory_down", "WheelDown")
 
-bind("player_input_look_up", "MouseUp")
-bind("player_input_look_down", "MouseDown")
-bind("player_input_look_right", "MouseRight")
-bind("player_input_look_left", "MouseLeft")
+#bind("player_input_jump", "Space")
+
+bind_controller_button("player_input_jump","Space",Constants.XBOX_A)
+#bind("player_input_crouch", "Control")
+
+bind_controller_button("player_input_crouch","Control",Constants.XBOX_RS_CLICK)
+
+#bind("player_input_inventory_up", "WheelUp")
+bind_controller_button("player_input_inventory_up","WheelUp",Constants.XBOX_LB)
+#bind("player_input_inventory_down", "WheelDown")
+bind_controller_button("player_input_inventory_down","WheelDown",Constants.XBOX_RB)
+
+#bind("player_input_look_up", "MouseUp")
+bind_controller_axis("player_input_look_up", "MouseUp", Constants.XBOX_RS_VERTICAL, Constants.JOYSTICK_UP)
+#bind("player_input_look_down", "MouseDown")
+bind_controller_axis("player_input_look_down", "MouseDown", Constants.XBOX_RS_VERTICAL, Constants.JOYSTICK_DOWN)
+#bind("player_input_look_right", "MouseRight")
+bind_controller_axis("player_input_look_right", "MouseRight", Constants.XBOX_RS_HORIZONTAL, Constants.JOYSTICK_RIGHT)
+#bind("player_input_look_left", "MouseLeft")
+bind_controller_axis("player_input_look_left", "MouseLeft", Constants.XBOX_RS_HORIZONTAL, Constants.JOYSTICK_LEFT)
 
 bind("player_input_build_rotate", "R")
 bind("player_input_drop", "Q")
 
-bind("player_position_reset", "K")
-bind("fly_toggle", "T")
+#bind("player_position_reset", "K")
+bind_controller_button("player_position_reset","K",Constants.XBOX_Y)
+#bind("fly_toggle", "T")
+bind_controller_button("fly_toggle","T",Constants.XBOX_LS_CLICK)
 
-bind("player_input_primary_fire", "MouseOne")
-bind("player_input_secondary_fire", "MouseTwo")
+#bind("player_input_primary_fire", "MouseOne")
+bind_controller_button("player_input_primary_fire","MouseOne",Constants.XBOX_RT)
+#bind("player_input_secondary_fire", "MouseTwo")
+bind_controller_button("player_input_secondary_fire","MouseTwo",Constants.XBOX_LT)
+
 
 chunk_render_distance(10)
 fps_max(200)
