71964825e692338a16f7e10cfc2c653146ccc04d
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2019-03-15 20:17:43 -0400

    Rename HistLocal int to better reflect usage

--- Scripting/Console.cs
+++ Scripting/Console.cs
@@ -12,7 +12,7 @@ public class Console : Node
 	private static RichTextLabel ConsoleLabel;
 	private static RichTextLabel LogLabel;
 	private static List<string> History = new List<string>();
-	private static int HistLocal = 0;
+	private static int HistoryLocation = 0;
 
 
 	private static Console Self;
@@ -40,10 +40,10 @@ public class Console : Node
 			GetTree().SetInputAsHandled();
 			InputLine.GrabFocus();
 
-			if(Input.IsActionJustPressed("ui_up") && HistLocal > 0)
+			if(Input.IsActionJustPressed("ui_up") && HistoryLocation > 0)
 			{
-				HistLocal -= 1;
-				InputLine.Text = History[HistLocal];
+				HistoryLocation -= 1;
+				InputLine.Text = History[HistoryLocation];
 
 				InputLine.CaretPosition = InputLine.Text.Length;
 			}
@@ -54,16 +54,16 @@ public class Console : Node
 			GetTree().SetInputAsHandled();
 			InputLine.GrabFocus();
 
-			if(Input.IsActionJustPressed("ui_down") && HistLocal < History.Count)
+			if(Input.IsActionJustPressed("ui_down") && HistoryLocation < History.Count)
 			{
-				HistLocal += 1;
-				if(HistLocal == History.Count)
+				HistoryLocation += 1;
+				if(HistoryLocation == History.Count)
 				{
 					InputLine.Text = "";
 				}
 				else
 				{
-					InputLine.Text = History[HistLocal];
+					InputLine.Text = History[HistoryLocation];
 				}
 
 				InputLine.CaretPosition = InputLine.Text.Length;
@@ -104,7 +104,7 @@ public class Console : Node
 		{
 			History.Add(Command);
 		}
-		HistLocal = History.Count;
+		HistoryLocation = History.Count;
 
 		Scripting.RunConsoleLine(Command);
 	}
@@ -120,7 +120,7 @@ public class Console : Node
 	{
 		Window.Close();
 		IsOpen = false;
-		HistLocal = History.Count;
+		HistoryLocation = History.Count;
 
 		if(!Menu.IsOpen)
 		{
@@ -134,7 +134,7 @@ public class Console : Node
 	{
 		Window.Open();
 		IsOpen = true;
-		HistLocal = History.Count;
+		HistoryLocation = History.Count;
 
 		Input.SetMouseMode(Input.MouseMode.Visible);
 		Game.BindsEnabled = false;
