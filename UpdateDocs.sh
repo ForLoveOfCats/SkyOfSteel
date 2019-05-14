2436f65af505bcb6783119bb9d6143abfc17f92d
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2019-05-14 10:51:08 -0400

    Fix paths in UpdateDocs and remove dry run commented out line

--- UpdateDocs.sh
+++ UpdateDocs.sh
@@ -5,22 +5,23 @@ COMMIT="$(git rev-parse HEAD)"
 mkdir ../GithubPages
 cd ../GithubPages
 git clone --depth 1 -b GithubPages --single-branch https://github.com/ForLoveOfCats/SkyOfSteel.git
+cd SkyOfSteel
 git checkout GithubPages #just in case
-cd ../SkyOfSteel
+cd ../../SkyOfSteel
 
 #then we build the docs from the original clone
 doxygen
 
 #almost done, next we copy the results into the copy
-cp -a -r ./Docs ../GithubPages/SkyOfSteel/Docs
+cp -a -r "./Docs" "../GithubPages/SkyOfSteel/Docs"
 
 #finally we commit and push to the branch
 cd ../GithubPages/SkyOfSteel
 git add * #track and stage any new files
 git stage * #stage any changed files
 git commit -m "Update docs for $COMMIT"
-#git push https://${GH_TOKEN}@github.com/ForLoveOfCats/SkyOfSteel.git TODO re-add this after dry run
+git push https://${GH_TOKEN}@github.com/ForLoveOfCats/SkyOfSteel.git
 
 #then just as some cleanup we cd back into the original
 #just in case this is being run locally
-cd ../SkyOfSteel
+cd ../../SkyOfSteel
