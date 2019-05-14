f1b928722533f53e784deebae10d4d1470cf5cc1
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2019-05-14 10:28:54 -0400

    Clone GithubPages branch separately when updating docs in CI (dry run)

--- UpdateDocs.sh
+++ UpdateDocs.sh
@@ -1,31 +1,25 @@
 #First we note the current commit
 COMMIT="$(git rev-parse HEAD)"
 
-#then we duplicate the entire repo
-cp -a -r ../SkyOfSteel ../GithubPages
-
-#then we checkout the correct branch in the copy
+#then we get the GithubPages branch
+mkdir ../GithubPages
 cd ../GithubPages
-pwd
-ls
-git fetch --all
-git checkout GithubPages
-git reset --hard
-git clean -d -f
+git clone --depth 1 -b GithubPages --single-branch https://github.com/ForLoveOfCats/SkyOfSteel.git
+git checkout GithubPages #just in case
 cd ../SkyOfSteel
 
 #then we build the docs from the original clone
 doxygen
 
 #almost done, next we copy the results into the copy
-cp -a -r ./Docs ../GithubPages/Docs
+cp -a -r ./Docs ../GithubPages/SkyOfSteel/Docs
 
 #finally we commit and push to the branch
-cd ../GithubPages
+cd ../GithubPages/SkyOfSteel
 git add * #track and stage any new files
 git stage * #stage any changed files
 git commit -m "Update docs for $COMMIT"
-git push https://${GH_TOKEN}@github.com/ForLoveOfCats/SkyOfSteel.git
+#git push https://${GH_TOKEN}@github.com/ForLoveOfCats/SkyOfSteel.git TODO re-add this after dry run
 
 #then just as some cleanup we cd back into the original
 #just in case this is being run locally
