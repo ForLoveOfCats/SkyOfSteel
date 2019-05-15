18e5baf8cd8b48dee7a1e51dd6f9cb52ccead541
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2019-05-15 16:51:47 -0400

    Add mouseover sfx and mouse cursor to file load buttons

--- UI/Menu/LoadWorldHost/LoadHostButton.cs
+++ UI/Menu/LoadWorldHost/LoadHostButton.cs
@@ -4,11 +4,22 @@ using System;
 public class LoadHostButton : Button
 {
 	public string SaveName = "";
+	public AudioStreamPlayer MouseOverSfx;
 
+	public override void _Ready()
+	{
+		MouseOverSfx = GetNode<AudioStreamPlayer>("MouseOverSfx");
+	}
 
 	public void LoadPressed()
 	{
 		Net.Host();
 		World.Load(SaveName);
+	}
+
+
+	public void MouseEnter()
+	{
+		MouseOverSfx.Play();
 	}
 }
