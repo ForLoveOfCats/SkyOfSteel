2ed093a2bed256734c7149fabf8ce14d65dabf03
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2019-05-15 16:44:44 -0400

    Add menu button mouseover sound effect

---
+++ UI/Menu/Pieces/ButtonPiece.cs
@@ -0,0 +1,18 @@
+using Godot;
+
+
+public class ButtonPiece : Button
+{
+	public AudioStreamPlayer MouseOverSfx;
+
+	public override void _Ready()
+	{
+		MouseOverSfx = GetNode<AudioStreamPlayer>("MouseOverSfx");
+	}
+
+
+	public void MouseEnter()
+	{
+		MouseOverSfx.Play();
+	}
+}
