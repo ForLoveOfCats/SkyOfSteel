77cb82ba2d75daea71c859efc3cb02956b2583cc
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2019-06-05 10:27:56 -0400

    Update documentation build script for new website repo

--- UpdateDocs.sh
+++ UpdateDocs.sh
@@ -1,27 +1,24 @@
 #First we note the current commit
 COMMIT="$(git rev-parse HEAD)"
 
-#then we get the gh-pages branch
-mkdir ../gh-pages
-cd ../gh-pages
-git clone --depth 1 -b gh-pages --single-branch https://github.com/ForLoveOfCats/SkyOfSteel.git
+#then we get the website repo
+cd ..
+git clone --depth 1 https://github.com/ForLoveOfCats/SiteOfSteel.git
 cd SkyOfSteel
-git checkout gh-pages #just in case
-cd ../../SkyOfSteel
 
-#then we build the docs from the original clone
+#then we build the docs
 doxygen
 
-#almost done, next we copy the results into the copy
-cp -a -r "./DoxygenOutput" "../gh-pages/SkyOfSteel/Docs/Master/"
+#almost done, next we copy the results into the site repo
+cp -a -r "./DoxygenOutput" "../SiteOfSteel//Docs/Master/"
 
-#finally we commit and push to the branch
-cd ../gh-pages/SkyOfSteel
+#finally we commit and push
+cd ../SiteOfSteel
 git add * #track and stage any new files
 git stage * #stage any changed files
 git commit -m "Update docs for $COMMIT"
-git push https://${GH_TOKEN}@github.com/ForLoveOfCats/SkyOfSteel.git
+git push https://${GH_TOKEN}@github.com/ForLoveOfCats/SiteOfSteel.git
 
 #then just as some cleanup we cd back into the original
 #just in case this is being run locally
-cd ../../SkyOfSteel
+cd ../SkyOfSteel
