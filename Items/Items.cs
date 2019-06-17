d1e0f14770eca0d6180752541293e9127bc566d4
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2019-06-17 19:51:43 -0400

    Move rocket jumper logic into separate class

--- Items/Items.cs
+++ Items/Items.cs
@@ -11,12 +11,6 @@ public class Items : Node
 	public delegate void UseItemDelegate(Instance Item, Player UsingPlayer);
 
 
-	public static float MaxRocketPush = 100;
-	public static float MaxRocketDistance = 30;
-	public static float RocketHorizontalMultiplyer = 1.1f;
-	public static float RocketVerticalDivisor = 1.7f;
-
-
 	public class Instance
 	{
 		public Items.ID Id = Items.ID.ERROR;
@@ -453,31 +447,7 @@ public class Items : Node
 		UseDelegates = new Dictionary<ID, UseItemDelegate>() {
 			{
 				ID.ROCKET_JUMPER,
-				new UseItemDelegate((Instance Item, Player UsingPlayer) => {
-						RayCast Cast = UsingPlayer.GetNode<RayCast>("SteelCamera/RocketRayCast");
-						if(Cast.IsColliding())
-						{
-							float Distance = UsingPlayer.CenterPosition().DistanceTo(Cast.GetCollisionPoint());
-							float Power = (MaxRocketPush/MaxRocketDistance) * Clamp(MaxRocketDistance - Distance, 0, MaxRocketDistance);
-
-							Camera Cam = UsingPlayer.GetNode<Camera>("SteelCamera");
-							Vector3 Push = new Vector3(0, 0, Power);
-							Push = Push.Rotated(new Vector3(1,0,0), Deg2Rad(LoopRotation(Cam.RotationDegrees.x)));
-							Push = Push.Rotated(new Vector3(0,1,0), Deg2Rad(LoopRotation(UsingPlayer.RotationDegrees.y + 180)));
-
-							{
-								Vector3 Flat = Push;
-								Flat.y = 0;
-								Flat *= RocketHorizontalMultiplyer;
-								Push.x = Flat.x;
-								Push.z = Flat.z;
-							}
-							Push.y /= RocketVerticalDivisor;
-
-							UsingPlayer.ApplyPush(Push);
-							UsingPlayer.RecoverPercentage = 0;
-						}
-					})
+				RocketJumper.Fire
 			}
 		};
 	}
---
+++ Items/Logic/RocketJumper.cs
@@ -0,0 +1,40 @@
+using Godot;
+using static Godot.Mathf;
+using static SteelMath;
+
+
+public class RocketJumper
+{
+	public static float MaxRocketPush = 100;
+	public static float MaxRocketDistance = 30;
+	public static float RocketHorizontalMultiplyer = 1.1f;
+	public static float RocketVerticalDivisor = 1.7f;
+
+
+	public static void Fire(Items.Instance Item, Player UsingPlayer)
+	{
+		RayCast Cast = UsingPlayer.GetNode<RayCast>("SteelCamera/RocketRayCast");
+		if(Cast.IsColliding())
+		{
+			float Distance = UsingPlayer.CenterPosition().DistanceTo(Cast.GetCollisionPoint());
+			float Power = (MaxRocketPush/MaxRocketDistance) * Clamp(MaxRocketDistance - Distance, 0, MaxRocketDistance);
+
+			Camera Cam = UsingPlayer.GetNode<Camera>("SteelCamera");
+			Vector3 Push = new Vector3(0, 0, Power);
+			Push = Push.Rotated(new Vector3(1,0,0), Deg2Rad(LoopRotation(Cam.RotationDegrees.x)));
+			Push = Push.Rotated(new Vector3(0,1,0), Deg2Rad(LoopRotation(UsingPlayer.RotationDegrees.y + 180)));
+
+			{
+				Vector3 Flat = Push;
+				Flat.y = 0;
+				Flat *= RocketHorizontalMultiplyer;
+				Push.x = Flat.x;
+				Push.z = Flat.z;
+			}
+			Push.y /= RocketVerticalDivisor;
+
+			UsingPlayer.ApplyPush(Push);
+			UsingPlayer.RecoverPercentage = 0;
+		}
+	}
+}
