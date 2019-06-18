5dae997d96fc9d13c72e85cd18ab50b2d0e956e7
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2019-06-18 17:17:00 -0400

    Lower air acceleration slightly

--- Player/Player.cs
+++ Player/Player.cs
@@ -14,7 +14,7 @@ public class Player : KinematicBody, IPushable
 	public float SprintMultiplyer = 2; //Speed while sprinting is base speed times this value
 	public float MaxMovementSpeed { get { return BaseMovementSpeed*SprintMultiplyer; } }
 	public float MaxVerticalSpeed = 100f;
-	public float AirAcceleration = 24; //How many units per second to accelerate
+	public float AirAcceleration = 22; //How many units per second to accelerate
 	public float DecelerateTime = 0.2f; //How many seconds needed to stop from full speed
 	public float Friction { get { return MaxMovementSpeed / DecelerateTime; } }
 	public float JumpSpeedMultiplyer = 15f;
