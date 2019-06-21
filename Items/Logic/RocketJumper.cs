847129b32aee83802a77e311786d53c284ef30f9
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2019-06-21 17:23:15 -0400

    Bump rocket travel speed

--- Items/Logic/RocketJumper.cs
+++ Items/Logic/RocketJumper.cs
@@ -5,7 +5,7 @@ using static SteelMath;
 
 public class RocketJumper : Node
 {
-	public static float RocketTravelSpeed = 120; //Units-per-second
+	public static float RocketTravelSpeed = 150; //Units-per-second
 	public static float RocketFuseTime = 4f; //In seconds
 	public static float MaxRocketPush = 70; //Units-per-second force applied
 	public static float MaxRocketDistance = 30; //Make sure that radius of AffectArea on JumperRocket matches
