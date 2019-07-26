88c6dfb18c56dea361e484b7e924df376110ea24
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2019-07-26 09:08:03 -0400

    Basic hitscan firearm raycasting

--- Items/Logic/Hitscan/Hitscan.cs
+++ Items/Logic/Hitscan/Hitscan.cs
@@ -1,4 +1,5 @@
 using Godot;
+using static Godot.Mathf;
 using static System.Diagnostics.Debug;
 
 
@@ -24,10 +25,30 @@ public class Hitscan : Spatial
 	}
 
 
-	public static void Fire(float VerticalRecoil, float HorizontalRecoil)
+	public static void Fire(float VerticalAngle, float HorizontalAngle, float Range)
 	{
 		Assert(NextRecoilDirection == 1 || NextRecoilDirection == -1);
+		Player Plr = Game.PossessedPlayer;
 
+		{
+			PhysicsDirectSpaceState State = Self.GetWorld().DirectSpaceState;
+
+			Vector3 Origin = Plr.Cam.GlobalTransform.origin;
+			Vector3 Endpoint = Origin + new Vector3(0, 0, Range)
+				.Rotated(new Vector3(1, 0, 0), Deg2Rad(-Plr.LookVertical - VerticalAngle))
+				.Rotated(new Vector3(0, 1, 0), Deg2Rad(Plr.LookHorizontal + HorizontalAngle));
+
+			Godot.Collections.Dictionary Results = State.IntersectRay(Origin, Endpoint, null, 1);
+			if(Results.Count > 0) //We hit something
+			{
+				World.DebugPlot((Vector3)Results["position"]);
+			}
+		}
+	}
+
+
+	public static void ApplyRecoil(float VerticalRecoil, float HorizontalRecoil)
+	{
 		Player Plr = Game.PossessedPlayer;
 		Plr.ApplyLookVertical(VerticalRecoil);
 		Plr.LookHorizontal -= HorizontalRecoil*NextRecoilDirection;
--- Items/Logic/Hitscan/Thunderbolt.cs
+++ Items/Logic/Hitscan/Thunderbolt.cs
@@ -5,6 +5,7 @@ public class Thunderbolt : Node
 {
 	public static float VerticalRecoil = 8;
 	public static float HorizontalRecoil = 3;
+	public static float Range = 500;
 	public static float FireCooldown = 40;
 
 	public static Thunderbolt Self;
@@ -19,7 +20,9 @@ public class Thunderbolt : Node
 
 	public static void Fire(Items.Instance Item, Player UsingPlayer)
 	{
-		Hitscan.Fire(VerticalRecoil, HorizontalRecoil);
+		Hitscan.Fire(0, 0, Range);
+		Hitscan.ApplyRecoil(VerticalRecoil, HorizontalRecoil);
+
 		UsingPlayer.SetCooldown(0, FireCooldown, true);
 		UsingPlayer.SfxManager.FpThunderboltFire();
 	}
