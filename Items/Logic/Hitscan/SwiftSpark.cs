144b9431851001909a7905ea1cc6c9a388ef41b2
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2019-08-23 09:07:11 -0400

    Rebalance the SwiftSpark

--- Items/Logic/Hitscan/SwiftSpark.cs
+++ Items/Logic/Hitscan/SwiftSpark.cs
@@ -3,13 +3,13 @@ using Godot;
 
 public static class SwiftSpark
 {
-	public static float VerticalRecoil = 4;
+	public static float VerticalRecoil = 2;
 	public static float HorizontalRecoil = 1;
 	public static float Range = 400;
-	public static float HeadshotDamage = 20;
-	public static float BodyshotDamage = 15;
-	public static float LegshotDamage = 10;
-	public static float FireCooldown = 14;
+	public static float HeadshotDamage = 25;
+	public static float BodyshotDamage = 20;
+	public static float LegshotDamage = 15;
+	public static float FireCooldown = 10;
 
 
 	public static void Fire(Items.Instance Item, Player UsingPlayer)
