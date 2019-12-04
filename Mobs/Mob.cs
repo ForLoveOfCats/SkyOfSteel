148256be909c05efb94ddb6c2a81e54701a545ed
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2019-12-04 15:53:42 -0500

    Vastly improve simple mob system

--- Mobs/Mob.cs
+++ Mobs/Mob.cs
@@ -8,68 +8,36 @@ public class Mob : KinematicBody
 {
 	public static Random RandomInstance = new Random();
 
-	public float TimeUntilNextMovement = 0;
 	public int TargetId = -1;
 
 
 	public override void _Process(float Delta)
 	{
-		TimeUntilNextMovement -= Delta;
-		if(TimeUntilNextMovement <= 0)
+		if(TargetId == -1 && World.Pathfinder.GetPointCount() <= 2)
+			return;
+		else if(TargetId == -1)
+			TargetId = (int)World.Pathfinder.GetPoints()[RandomInstance.Next(World.Pathfinder.GetPointCount())];
+
+		int ClosestId = World.Pathfinder.GetClosestPoint(Translation);
+		if(ClosestId == TargetId)
 		{
-			TimeUntilNextMovement = RandomInstance.Next(10, 15);
-
-			if(World.Pathfinder.GetPointCount() <= 0)
-				return;
-
-			int ClosestId = World.Pathfinder.GetClosestPoint(Translation);
-
-			int IterationCount = 0;
-			int LocalTargetId = ClosestId;
-			while(LocalTargetId == ClosestId)
-			{
-				IterationCount += 1;
-				if(IterationCount > 10)
-				{
-					TimeUntilNextMovement = RandomInstance.Next(0, 1);
-					return;
-				}
-
-				LocalTargetId = (int)World.Pathfinder.GetPoints()[RandomInstance.Next(World.Pathfinder.GetPointCount())];
-			}
-			TargetId = LocalTargetId;
-
-			// int[] Path = World.Pathfinder.GetIdPath(ClosestId, TargetId);
-			// GD.Print($"Closest: {ClosestId}, Closest connection count: {World.Pathfinder.GetPointConnections(ClosestId).Length}, TargetId: {TargetId}, Path count: {Path.Length}");
+			TargetId = -1;
+			return;
 		}
 
+		int[] Path = World.Pathfinder.GetIdPath(ClosestId, TargetId);
+		if(Path.Length >= 2)
 		{
-			if(TargetId == -1 || World.Pathfinder.GetPointCount() <= 2)
-				return;
-
-			int ClosestId = World.Pathfinder.GetClosestPoint(Translation);
-			if(ClosestId == TargetId)
-			{
-				TimeUntilNextMovement = RandomInstance.Next(0, 1);
-				TargetId = -1;
-				return;
-			}
-
-			int[] Path = World.Pathfinder.GetIdPath(ClosestId, TargetId);
-			if(Path.Length > 2)
-			{
-				Vector3 SubTargetVec = World.Pathfinder.GetPointPosition(Path[1]);
-				MoveAndSlide(
-					ClampVec3(
-						SubTargetVec-Translation/* + new Vector3(
-							((float)RandomInstance.Next(-40, 40))/5f,
-							0,
-							((float)RandomInstance.Next(-40, 40))/5f)*/,
-						32,
-						32
+			Vector3 SubTargetVec = World.Pathfinder.GetPointPosition(Path[1]);
+			MoveAndSlide(
+				ClampVec3(
+					SubTargetVec-Translation,
+					32,
+					32
 					)
-				);
-			}
+			);
 		}
+		else
+			TargetId = -1;
 	}
 }
